503d07ce7ef68123ffa805592a518a8b99638e89
duffyj2 <duffyj2@tcd.ie>
2015-09-14 16:30:26 +0100

    Small changes, nomenclature, testing  etc.

--- strain.py
+++ strain.py
@@ -8,24 +8,25 @@ Ssigma = 0.165		# Poisson's ration in graphene
 Salpha = 3.37		# A strain constant, taken from the literature
 
 
-def SHopping(ratio1,ratio2):
+def SHoppingGen(ratio1,ratio2):
   """Calculates the hopping integrals t1, t2 given the ratio of the relevant bond lengths to the pristine length."""
   t1 = t*expRe(-Salpha*(ratio1-1.0))
   t2 = t*expRe(-Salpha*(ratio2-1.0))
   return t1, t2
 
-def strainZ(Seps,Ssigma):
+
+def SHoppingZ(Seps,Ssigma):
   """Gets the correct bond length ratios for zigzag strain"""
   ratio1 = 1.0 + 3.0*Seps/4.0 - Seps*Ssigma/4.0	# R1/R0
   ratio2 = 1.0-Seps*Ssigma	# R2/R0
-  return ratio1, ratio2
+  return SHoppingGen(ratio1,ratio2)
 
 
-def strainA(Seps,Ssigma):
+def SHoppingA(Seps,Ssigma):
   """Gets the correct bond length ratios for armchair strain"""
   ratio1 = 1.0 + Seps/4.0 - 3.0/4.0*Seps*Ssigma	# R1/R0
   ratio2 = 1.0-Seps*Ssigma	# R2/R0
-  return ratio1, ratio2
+  return SHoppingGen(ratio1,ratio2)
 
 
 def gSBulkDouble(m,n,s,E):
@@ -50,26 +51,6 @@ def gSBulkDouble(m,n,s,E):
   gr = dblquad(gIRe, -pi/2.0, pi/2.0, lambda kZ: -pi, lambda kZ: pi)[0]
   gim = dblquad(gIIm, -pi/2.0, pi/2.0, lambda kZ: -pi, lambda kZ: pi)[0]
   return gr + 1j*gim
-
-
-def gSGNR(nE,m1,n1,m2,n2,s,E):
-  """Calculates the GF of a GNR for a strained system, without the integral being performed analytically"""
-  def gI(kA):  
-    if s == 0:
-      N = E
-    elif s == 1:
-      N = t2 + 2*t1*exp(1j*kA)*cos(pi*j/nE)
-    else:
-      N = t2 + 2*t1*exp(-1j*kA)*cos(pi*j/nE)
-    Beps2 = t2**2+4*t1*t2*cos(kA)*cos(pi*j/nE)+4*t1**2 *cos(pi*j/nE)**2
-    
-    g = N*exp(1j*kA*(m2+n2-m1-n1))*sin(pi*j*(m1-n1)/nE)*sin(pi*j*(m2-n2)/nE)/(E**2-Beps2)
-    return g
-  
-  g = 0
-  for j in range(1,nE):
-    g += C_int(gI, -pi, pi)
-  return g/(pi*nE)
 
 
 def gSBulk(m,n,s,E):
@@ -99,24 +80,96 @@ def gSBulk(m,n,s,E):
   return C_int(gI,-pi/2,pi/2)
 
 
-if __name__ == "__main__":
-  s = 1
-  ratio1,ratio2 = strainZ(Seps,Ssigma)
-  t1,t2 = SHopping(ratio1,ratio2)
+def gSGNR(nE,m1,n1,m2,n2,s,E):
+  """Calculates the GF of a GNR for a strained system, without the integral being performed analytically"""
+  def gI(kA):  
+    if s == 0:
+      N = E
+    elif s == 1:
+      N = t2 + 2*t1*exp(1j*kA)*cos(pi*j/nE)
+    else:
+      N = t2 + 2*t1*exp(-1j*kA)*cos(pi*j/nE)
+    Beps2 = t2**2+4*t1*t2*cos(kA)*cos(pi*j/nE)+4*t1**2 *cos(pi*j/nE)**2
+    
+    g = N*exp(1j*kA*(m2+n2-m1-n1))*sin(pi*j*(m1-n1)/nE)*sin(pi*j*(m2-n2)/nE)/(E**2-Beps2)
+    return g
   
-  m,n = 20,40
-  Elist = np.linspace(-0.5+1j*eta,0.5+1j*eta,201)
-  g1list = np.array([gSBulkDouble(m,n,s,E) for E in Elist])
-  g2list = np.array([gSBulk(m,n,s,E) for E in Elist])
-  pl.plot(Elist,g1list.real)
-  pl.plot(Elist,g1list.imag)
-  pl.plot(Elist,g2list.real,'o')
-  pl.plot(Elist,g2list.imag,'o')
-  pl.show()
-  #Elist = np.linspace(-0.3+1j*eta,0.3+1j*eta,201)
-  #glist = np.array([gSBulk(m,n,s,E) for E in Elist])
-  #pl.plot(Elist,glist.real)
-  #pl.plot(Elist,glist.imag)
+  g = 0
+  for j in range(1,nE):
+    g += C_int(gI, -pi, pi)
+  return g/(pi*nE)
+
+
+def gTest(nE,m1,n1,m2,n2,s,E):
+  """Calculates the GF for a nanoribbon, want to incorporate strain at some point"""
+  def gterm(ky):
+    q = acos( (E**2 - t**2 - 4.0*t**2 *cos(ky)**2)/(4.0*t**2 *cos(ky) ) )  
+    if q.imag < 0.0: q = -q
+
+    Const = 1j/(2.0*nE*t**2)
+    Den = cos(ky)*sin(q)
+
+    if s == 0:
+      sig = copysign(1,m2+n2-m1-n1)
+      return Const*E*exp( 1j*sig*q*(m2+n2-m1-n1) )*sin(ky*(m2-n2))*sin(ky*(m1-n1))/Den 
+    elif s == 1:
+      sig = copysign(1,m2+n2-m1-n1)
+      f = 1.0 + 2.0*cos(ky)*exp(sig*1j*q)
+      return Const*t*f*exp( 1j*sig*q*(m2+n2-m1-n1) )*sin(ky*(m2-n2))*sin(ky*(m1-n1))/Den 
+    elif s == -1:
+      sig = copysign(1,m2+n2-m1-n1-1)
+      ft = 1.0 + 2.0*cos(ky)*exp(-sig*1j*q)
+      return Const*t*ft*exp( 1j*sig*q*(m2+n2-m1-n1) )*sin(ky*(m2-n2))*sin(ky*(m1-n1))/Den 
+    else:
+      print 'Sublattice error in gTest'
+  
+  def limit_term(ky):
+    if s == 0:
+      N_ab = E
+    elif (s == 1) or (s == -1):
+      N_ab = t
+    else:
+      print 'Sublattice error in gTest'
+      
+    return 2.0*N_ab*sin(ky*(m2-n2))*sin(ky*(m1-n1))/( nE*( E**2 - t**2 ) )
+  
+  g = 0.0
+  if nE % 2 == 0:
+    for j in range(1,nE):
+      if j == nE/2: continue		# Avoid singularities
+      g += gterm(pi*j/nE)
+    if m2+n2-m1-n1 == 0:
+      g += limit_term(pi/2)
+  else: 
+    for j in range(1,nE):
+      g += gterm(pi*j/nE)
+      
+  return g
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+  t1 = t2 = t
+  for nE,m1,n1,m2,n2,s,E in [[6,1,-1,4,2,0,-0.5+1j*eta],[7,7,4,0,-1,1,-0.5+1j*eta],[9,1,0,3,0,-1,-0.5+1j*eta],[4,0,-1,0,-1,1,-0.5+1j*eta],[3,0,-2,0,-1,0,-0.5+1j*eta]]:
+    print gSGNR(nE,m1,n1,m2,n2,s,E)
+    print gTest(nE,m1,n1,m2,n2,s,E)
+    print 
+  
+  
+  #s = 1
+  #t1,t2 = SHoppingZ(Seps,Ssigma)  
+  #m,n = 20,40
+  #Elist = np.linspace(-0.5+1j*eta,0.5+1j*eta,201)
+  #g1list = np.array([gSBulkDouble(m,n,s,E) for E in Elist])
+  #g2list = np.array([gSBulk(m,n,s,E) for E in Elist])
+  #pl.plot(Elist,g1list.real)
+  #pl.plot(Elist,g1list.imag)
+  #pl.plot(Elist,g2list.real,'o')
+  #pl.plot(Elist,g2list.imag,'o')
   #pl.show()
 
   
